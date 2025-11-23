965b9fd9c80f1ade2e553da72531bbf248102fee
Alvaro-36 <lopezalvaro456123@gmail.com>
2025-11-23 13:07:45 -0300

    Implementado el metodo isMutant()

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.example.service.MutantDetector;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+//@EntityScan("com.utn.productos.model")
+//@EnableJpaRepositories("com.utn.productos.repository")
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        System.out.println("HOLA MUNDO");
+
+        String[] testMatrix = {
+                "h00a",
+                "00a0",
+                "0a00",
+                "a00s"
+        };
+        MutantDetector detector = MutantDetector.builder().build();
+        detector.isMutant(testMatrix);
+
+
     }
+
 }
---
+++ src/main/java/org/example/service/MutantDetector.java
@@ -0,0 +1,102 @@
+package org.example.service;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@Builder
+@Data
+public class MutantDetector {
+
+    public boolean isMutant(String[] dna) {
+        int filas = dna.length;
+
+
+        if (dna == null || dna.length == 0) {
+            return false;  // ADN inválido
+        }
+
+        for (String row : dna) {
+            if (row == null || row.length() != filas) {
+                return false;  // No es matriz NxN
+            }
+
+            for (char c : row.toCharArray()) {
+                if (c != 'A' && c != 'T' && c != 'C' && c != 'G') {
+                    return false;  // Carácter inválido
+                }
+            }
+        }
+
+
+        char[][] dnaMatrix = new char[filas][];
+
+        for (int i = 0; i < filas; i++) {
+            dnaMatrix[i] = dna[i].toCharArray();
+        }
+        int columnas = dnaMatrix[0].length;
+        int contadorPatrones = 0;
+
+        System.out.println(filas);
+        System.out.println(columnas);
+
+        int i;
+        //Busqueda Horizontal
+        i = 0;
+        while (i < filas && contadorPatrones < 2) {
+            int j = 0;
+            while (j < columnas - 3 && contadorPatrones < 2) {
+                if (this.areEqual(dnaMatrix[i][j], dnaMatrix[i][j + 1], dnaMatrix[i][j + 2], dnaMatrix[i][j + 3])) {
+                    contadorPatrones++;
+                }
+                j++;
+            }
+            i++;
+        }
+        //Busqueda vertical
+        i = 0;
+        while (i < filas-3 && contadorPatrones < 2) {
+            int j = 0;
+            while (j < columnas && contadorPatrones < 2) {
+                if (this.areEqual(dnaMatrix[i][j], dnaMatrix[i+1][j], dnaMatrix[i+2][j], dnaMatrix[i+3][j])) {
+                    contadorPatrones++;
+                }
+                j++;
+            }
+            i++;
+        }
+        //Busqueda oblicua \
+        i = 0;
+        while (i < filas-3 && contadorPatrones < 2) {
+            int j = 0;
+            while (j < columnas-3 && contadorPatrones < 2) {
+                if (this.areEqual(dnaMatrix[i][j], dnaMatrix[i+1][j+1], dnaMatrix[i+2][j+2], dnaMatrix[i+3][j+3])) {
+                    contadorPatrones++;
+                }
+            j++;
+            }
+        i++;
+        }
+        //Busqueda oblicua /
+        i = 0;
+        while (i < filas-3 && contadorPatrones < 2) {
+            int j = 3;
+            while (j < columnas && contadorPatrones < 2) {
+                if (this.areEqual(dnaMatrix[i][j], dnaMatrix[i+1][j-1], dnaMatrix[i+2][j-2], dnaMatrix[i+3][j-3])) {
+                    contadorPatrones++;
+                }
+                j++;
+            }
+            i++;
+        }
+
+
+        System.out.println(contadorPatrones);
+        return contadorPatrones > 1;// Recuerda devolver true solo si hay MÁS de 1 secuencia
+    }
+    private boolean areEqual(char a, char b, char c, char d) {
+        return a == b && a == c && a == d;
+    }
+}
