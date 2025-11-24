5a73b472edf0f80bfbf20da3b8c479b76ae7ea4e
Alvaro <lopezalvaro456123@gmail.com>
2025-11-24 01:02:19 -0300

    arreglos menores y añadido application.properties

--- src/main/java/org/example/controller/MutantController.java
+++ src/main/java/org/example/controller/MutantController.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import org.example.dto.DnaRequestDTO;
 import org.example.dto.ResponseStatsDTO;
 import org.example.service.MutantDetector;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/mutant")
 @Tag(name ="Controlador de Mutantes")
+
 public class MutantController {
 
     private final MutantService mutantService;
@@ -31,7 +33,7 @@ public class MutantController {
     @Operation(summary = "Comprueba si el adn ingresado es de un mutante y devuelve un booleano")
     @ApiResponse(responseCode = "200", description = "El ADN pertenece a un mutante")
     @ApiResponse(responseCode = "403", description = "El ADN no pertenece a un mutante")
-    @PostMapping("/")
+    @PostMapping
     public ResponseEntity<Void> checkMutant(@RequestBody @Valid DnaRequestDTO dnaRequest){
         return mutantService.analyzeDna(dnaRequest.getDna()) ? ResponseEntity.ok().build() : ResponseEntity.status(403).build();
     }
